d4e5f723d79e6e9979e40a8da4d02b1877042c3b
VictorLM20 <zx23student3410@colegiovalledelmiro.es>
2024-10-02 09:16:10 +0200

    Funcion de dividir añadida

--- calculadora.py
+++ calculadora.py
@@ -1,9 +1,18 @@
-def sumas(a, b):
+def sumar(a, b):
     return a + b
-
-print(sumas(3, 5))
 
 def restar(a, b):
     return a - b
 
-print(restar(10, 2))
+def multiplicar(a, b):
+    return a * b
+
+def dividir(a, b):
+    if b == 0:
+        return "Error: División por cero"
+    return a / b
+
+print(sumar(3, 5))
+print(restar(8, 2))
+print(multiplicar(4, 3))
+print(dividir(10, 2))
